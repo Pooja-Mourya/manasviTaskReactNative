3bfa5efbf2bfa26c161a4b2d688c6c688c6a6873
pooja.vishwakarma <poojaviswakarma687@gmail.com>
2024-04-07 13:20:50 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import {StyleSheet, Image} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createStackNavigator} from '@react-navigation/stack';
+import ProductBrowsingScreen from './src/screen/ProductBrowsingScreen';
+import CartScreen from './src/screen/CartScreen';
+import UserProfile from './src/screen/UserProfile';
+import ProductDetail from './src/screen/ProductDetail'; // Example additional screen
+
+const Tab = createBottomTabNavigator();
+const Stack = createStackNavigator();
+
+function HomeStack() {
+  return (
+    <Stack.Navigator
+      screenOptions={{
+        headerShown: false,
+      }}>
+      <Stack.Screen name="HomeStack" component={ProductBrowsingScreen} />
+      <Stack.Screen name="ProductDetail" component={ProductDetail} />
+    </Stack.Navigator>
+  );
+}
+
+function CartStack() {
+  return (
+    <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="CartStack" component={CartScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function UserStack() {
+  return (
+    <Stack.Navigator
+    screenOptions={{
+      headerShown: false,
+    }}>
+      <Stack.Screen name="UserStack" component={UserProfile} />
+    </Stack.Navigator>
+  );
+}
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={{
+          headerShown: false,
+          tabBarActiveTintColor: '#e91e63',
+          tabBarInactiveTintColor: 'gray',
+          tabBarStyle: {
+            backgroundColor: 'white',
+          },
+        }}>
+        <Tab.Screen
+          name="Home"
+          component={HomeStack}
+          options={{
+            tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <Image
+                style={styles.tinyLogo}
+                source={require('./src/assets/icon/home.png')}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Cart"
+          component={CartStack}
+          options={{
+            tabBarLabel: 'Cart',
+            tabBarIcon: ({color, size}) => (
+              <Image
+                style={styles.tinyLogo}
+                source={require('./src/assets/icon/grocery-store.png')}
+              />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="User"
+          component={UserStack}
+          options={{
+            tabBarLabel: 'User',
+            tabBarIcon: ({color, size}) => (
+              <Image
+                style={styles.tinyLogo}
+                source={require('./src/assets/icon/profile-user.png')}
+              />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+
+const styles = StyleSheet.create({
+  tinyLogo: {
+    width: 30,
+    height: 30,
+  },
+});
---
+++ src/screen/ProductBrowsingScreen.js
@@ -0,0 +1,177 @@
+import React, { useState, useEffect } from 'react';
+import { View, Text, Image, StyleSheet, TextInput, Button, TouchableOpacity, FlatList } from 'react-native';
+import axios from 'axios';
+import { createStackNavigator } from '@react-navigation/stack'; // Import stack navigator
+import Colors from '../assets/Colors';
+import ProductDetail from './ProductDetail'; // Import the product detail screen
+
+const Stack = createStackNavigator(); // Create a stack navigator
+
+const allCategory = [
+  {
+    id: '1',
+    category: 'Handbags',
+  },
+  {
+    id: '2',
+    category: 'Dresses',
+  },
+  {
+    id: '3',
+    category: 'Jewelry',
+  },
+  {
+    id: '4',
+    category: 'Electronics',
+  },
+  {
+    id: '5',
+    category: 'Woman',
+  },
+];
+
+function ProductBrowsingScreen({ navigation }) {
+  const [products, setProducts] = useState([]);
+  const [searchQuery, setSearchQuery] = useState('');
+
+  useEffect(() => {
+    fetchProducts();
+  }, []);
+
+  const fetchProducts = async () => {
+    try {
+      const response = await axios.get('https://fakestoreapi.com/products');
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  const handleSearch = async () => {
+    try {
+      const response = await axios.get(`https://fakestoreapi.com/products?search=${searchQuery}`);
+      setProducts(response.data);
+    } catch (error) {
+      console.error('Error searching products:', error);
+    }
+  };
+
+  const navigateToProductDetail = (productId) => {
+    navigation.navigate('ProductDetail', { productId });
+  };
+
+  const renderItem = ({ item }) => (
+    <TouchableOpacity
+      style={styles.productContainer}
+      onPress={() => navigateToProductDetail(item.id)}
+    >
+      <Image source={{ uri: item.image }} style={styles.image} />
+      <Text>{item.title}</Text>
+      <Text>{item.price}</Text>
+    </TouchableOpacity>
+  );
+
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="ProductList" options={{ headerShown: false }}>
+        {() => (
+          <View style={styles.mainContainer}>
+            <View style={styles.searchContainer}>
+              <TextInput
+                style={styles.input}
+                placeholder="Search products"
+                onChangeText={(text) => setSearchQuery(text)}
+                value={searchQuery}
+              />
+              <Button title="Search" onPress={handleSearch} />
+            </View>
+            <View style={styles.categoryContainer}>
+              <Text style={styles.allText} onPress={fetchProducts}>All</Text>
+              <FlatList
+                data={allCategory}
+                renderItem={({ item }) => (
+                  <TouchableOpacity style={styles.categoryItem}>
+                    <Text style={styles.categoryText}>{item.category}</Text>
+                  </TouchableOpacity>
+                )}
+                keyExtractor={(item) => item.id}
+                horizontal
+              />
+            </View>
+            <FlatList
+              data={products}
+              renderItem={renderItem}
+              keyExtractor={(item) => item.id.toString()}
+              numColumns={2}
+              contentContainerStyle={styles.productList}
+            />
+          </View>
+        )}
+      </Stack.Screen>
+      <Stack.Screen name="ProductDetail" component={ProductDetail} />
+    </Stack.Navigator>
+  );
+}
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    flex: 1,
+    padding: 10,
+  },
+  searchContainer: {
+    flexDirection: 'row',
+    marginBottom: 10,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    marginRight: 10,
+    paddingHorizontal: 10,
+  },
+  productContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 10,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    borderRadius: 5,
+    padding: 10,
+  },
+  image: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+    marginBottom: 5,
+  },
+  categoryContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  allText: {
+    backgroundColor: Colors.black,
+    color: Colors.white,
+    padding: 10,
+    borderRadius: 10,
+    marginRight: 10,
+  },
+  categoryItem: {
+    borderWidth: 1,
+    borderColor: 'black',
+    borderRadius: 10,
+    paddingHorizontal: 15,
+    paddingVertical: 10,
+    marginRight: 10,
+  },
+  categoryText: {
+    fontWeight: '600',
+  },
+  productList: {
+    flexGrow: 1,
+  },
+});
+
+export default ProductBrowsingScreen;
